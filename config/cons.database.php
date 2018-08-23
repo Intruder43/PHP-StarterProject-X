d5c368972e49bcb259166e530f8926ba07111ebf
I 'am me <jonime43@gmail.com>
2018-08-23 11:00:36 +0800

    Update cons.database.php

--- config/cons.database.php
+++ config/cons.database.php
@@ -3,7 +3,7 @@
 define('DB_TYPE', 'mysql');
 
 define('DBMK_HOST', 'localhost');
-define('DBMK_NAME', 'ntc_maintenance');
-define('DBMK_USER', 'ric202');
-define('DBMK_PASS', 'test');
+define('DBMK_NAME', 'Database Name');
+define('DBMK_USER', 'TEST');
+define('DBMK_PASS', 'TEST');
 
